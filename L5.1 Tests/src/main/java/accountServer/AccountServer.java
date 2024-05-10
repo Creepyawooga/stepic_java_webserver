f3d2cece4e4151f93362ad4e51f4f18eab5658a6
Кирилл Евстигнеев <creepyawooga.ev@gmail.com>
2024-05-10 15:24:26 +0500

    5

--- L5.1 Tests/src/main/java/accountServer/AccountServer.java
+++ L5.1 Tests/src/main/java/accountServer/AccountServer.java
@@ -1,16 +1,14 @@
 package accountServer;
 
-/**
- * @author a.akbashev
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
-public class AccountServer implements AccountServerI {
+import javax.management.AttributeChangeNotification;
+import javax.management.MBeanNotificationInfo;
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
+
+public class AccountServer extends NotificationBroadcasterSupport implements AccountServerI, AccountServerMBean {
     private int usersCount;
     private int usersLimit;
+    private long sequenceNumber = 1;
 
     public AccountServer(int usersLimit) {
         this.usersCount = 0;
@@ -35,10 +33,22 @@ public class AccountServer implements AccountServerI {
     @Override
     public void setUsersLimit(int usersLimit) {
         this.usersLimit = usersLimit;
+        Notification notification = new AttributeChangeNotification(this, sequenceNumber++, System.currentTimeMillis(),
+                "Users limit changed", "Users limit", "int", this.usersLimit, usersLimit);
+        sendNotification(notification);
     }
 
     @Override
     public int getUsersCount() {
         return usersCount;
+    }
+
+    @Override
+    public MBeanNotificationInfo[] getNotificationInfo() {
+        String[] types = new String[] {AttributeChangeNotification.ATTRIBUTE_CHANGE};
+        String name = AttributeChangeNotification.class.getName();
+        String description = "An attribute of this MBean has changed";
+        MBeanNotificationInfo info = new MBeanNotificationInfo(types, name, description);
+        return new MBeanNotificationInfo[] {info};
     }
 }
