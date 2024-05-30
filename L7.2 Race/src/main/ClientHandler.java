4959dc85b81c8ef22d657675d9c2ef7510d30b6c
Кирилл Евстигнеев <creepyawooga.ev@gmail.com>
2024-05-30 14:08:36 +0500

    07

---
+++ L7.2 Race/src/main/ClientHandler.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+            String message;
+            while ((message = in.readLine()) != null) {
+                if (message.equals("Bue.")) {
+                    break;
+                }
+                out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
